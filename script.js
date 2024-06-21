c46febaf9644299602b51d453ecb735ed6a4bb05
Ubaid Zain <uk348152@gmail.com>
2024-06-21 23:56:39 +0500

    Add infinite platforms

--- script.js
+++ script.js
@@ -10,40 +10,93 @@ let platformCount = 5;
 const platformGap = 630 / 5;
 let platformBottom;
 let platformLeft;
+let doodlerBottom;
+let doodlerLeft;
+let startingPoint = 65;
 let platforms = [];
+// let isFalling = true;
 
 // Selectors }
 
 // {Event handlers
+
 document.addEventListener("keydown", startGame);
 
 // Event handlers}
 
 // {Functions
 
-// setInterval(movePlatforms, 30)
+function movePlatforms() {
+  if (currentlyPlaying && doodlerBottom > 200) {
+    platforms.forEach((obj) => {
+      obj.bottom -= 4;
+      obj.platform.style.bottom = obj.bottom + "px";
+    });
+  }
+}
 
-// function movePlatforms ()  {
-// if (currentlyPlaying && doodler.style.bottom > )
-// }
+function addDeletePlatform() {
+  platforms.forEach((obj) => {
+    if (obj.bottom < 0) {
+      const platform = document.createElement("div");
+      let platformObject = {};
+      platform.classList.add("platform");
+      platformBottom = 50 + platformCount * platformGap;
+      platformObject.bottom = platformBottom;
+      platform.style.bottom = platformBottom + "px";
+      platformLeft = Math.random() * 410;
+      platformObject.left = platformLeft;
+      platform.style.left = platformLeft + "px";
+      platformObject.platform = platform;
+      obj.platform.remove();
+      platforms.shift();
+      platforms.push(platformObject);
+      gameContainer.appendChild(platform);
+    }
+  });
+}
+
+// let downTime = setInterval(() => {
+//   doodlerBottom -= 20;
+//   doodler.style.bottom = doodlerBottom + "px";
+// }, 30);
+function jump() {
+  platforms.forEach((obj) => {
+    if (
+      doodlerBottom <= obj.bottom + 15 &&
+      doodlerBottom >= obj.bottom &&
+      doodlerLeft <= obj.left + 90 &&
+      doodlerLeft + 80 >= obj.left
+    ) {
+      setInterval(() => {
+        doodlerBottom += 20;
+        doodler.style.bottom = doodlerBottom + "px";
+      }, 30);
+    }
+  });
+}
 
 function createDoodler() {
   doodler.classList.add("doodler");
-  doodler.style.left = platforms[0].style.left;
-  doodler.style.bottom = 50 + 15 + "px";
+  doodlerLeft = platforms[0].left;
+  doodler.style.left = doodlerLeft + "px";
+  doodlerBottom = 50 + 15;
+  doodler.style.bottom = doodlerBottom + "px";
   gameContainer.appendChild(doodler);
-  console.log(doodler.style.bottom);
 }
 function createPlatforms() {
   for (let i = 0; i < platformCount; i++) {
     const platform = document.createElement("div");
+    let platformObject = {};
     platform.classList.add("platform");
     platformBottom = 50 + i * platformGap;
+    platformObject.bottom = platformBottom;
     platform.style.bottom = platformBottom + "px";
     platformLeft = Math.random() * 410;
+    platformObject.left = platformLeft;
     platform.style.left = platformLeft + "px";
-    platforms.push(platform);
-    console.log(platforms);
+    platformObject.platform = platform;
+    platforms.push(platformObject);
 
     gameContainer.appendChild(platform);
   }
@@ -56,6 +109,9 @@ function startGame(e) {
     createDoodler();
 
     currentlyPlaying = true;
+    setInterval(movePlatforms, 30);
+    setInterval(addDeletePlatform, 30);
+    setInterval(jump, 30);
   }
 }
 // Functions}
