9b0f5f51cb5a7af8c4b3d817ab47bbcb944234c9
Ubaid Zain <uk348152@gmail.com>
2024-06-22 13:41:57 +0500

    Add jump mechanism

--- script.js
+++ script.js
@@ -14,17 +14,56 @@ let doodlerBottom;
 let doodlerLeft;
 let startingPoint = 65;
 let platforms = [];
-// let isFalling = true;
+let upTime;
+let downTime;
+let isFalling = true;
 
 // Selectors }
 
 // {Event handlers
 
 document.addEventListener("keydown", startGame);
+document.addEventListener("keydown", moveDoodler);
 
 // Event handlers}
 
 // {Functions
+function startGame(e) {
+  if (e.key === "Enter" && currentlyPlaying === false) {
+    message.innerHTML = "";
+    createPlatforms();
+    createDoodler();
+    currentlyPlaying = true;
+    setInterval(movePlatforms, 30);
+    setInterval(addDeletePlatform, 30);
+    jump();
+  }
+}
+function createDoodler() {
+  doodler.classList.add("doodler");
+  doodlerLeft = platforms[0].left;
+  doodler.style.left = doodlerLeft + "px";
+  doodlerBottom = 50 + 15;
+  doodler.style.bottom = doodlerBottom + "px";
+  gameContainer.appendChild(doodler);
+}
+function createPlatforms() {
+  for (let i = 0; i < platformCount; i++) {
+    const platform = document.createElement("div");
+    let platformObject = {};
+    platform.classList.add("platform");
+    platformBottom = 50 + i * platformGap;
+    platformObject.bottom = platformBottom;
+    platform.style.bottom = platformBottom + "px";
+    platformLeft = Math.random() * 410;
+    platformObject.left = platformLeft;
+    platform.style.left = platformLeft + "px";
+    platformObject.platform = platform;
+    platforms.push(platformObject);
+
+    gameContainer.appendChild(platform);
+  }
+}
 
 function movePlatforms() {
   if (currentlyPlaying && doodlerBottom > 200) {
@@ -56,62 +95,38 @@ function addDeletePlatform() {
   });
 }
 
-// let downTime = setInterval(() => {
-//   doodlerBottom -= 20;
-//   doodler.style.bottom = doodlerBottom + "px";
-// }, 30);
 function jump() {
-  platforms.forEach((obj) => {
-    if (
-      doodlerBottom <= obj.bottom + 15 &&
-      doodlerBottom >= obj.bottom &&
-      doodlerLeft <= obj.left + 90 &&
-      doodlerLeft + 80 >= obj.left
-    ) {
-      setInterval(() => {
-        doodlerBottom += 20;
-        doodler.style.bottom = doodlerBottom + "px";
-      }, 30);
+  clearInterval(downTime);
+  upTime = setInterval(() => {
+    doodlerBottom += 20;
+    doodler.style.bottom = doodlerBottom + "px";
+    if (doodlerBottom >= startingPoint + 200) {
+      isFalling = true;
+      fall();
     }
-  });
+  }, 30);
+}
+function fall() {
+  clearInterval(upTime);
+  downTime = setInterval(() => {
+    doodlerBottom -= 20;
+    doodler.style.bottom = doodlerBottom + "px";
+
+    platforms.forEach((obj) => {
+      if (
+        doodlerBottom <= obj.bottom + 15 &&
+        doodlerBottom >= obj.bottom &&
+        doodlerLeft <= obj.left + 90 &&
+        doodlerLeft + 80 >= obj.left &&
+        isFalling
+      ) {
+        startingPoint = doodlerBottom;
+        isFalling = false;
+
+        jump();
+      }
+    });
+  }, 30);
 }
 
-function createDoodler() {
-  doodler.classList.add("doodler");
-  doodlerLeft = platforms[0].left;
-  doodler.style.left = doodlerLeft + "px";
-  doodlerBottom = 50 + 15;
-  doodler.style.bottom = doodlerBottom + "px";
-  gameContainer.appendChild(doodler);
-}
-function createPlatforms() {
-  for (let i = 0; i < platformCount; i++) {
-    const platform = document.createElement("div");
-    let platformObject = {};
-    platform.classList.add("platform");
-    platformBottom = 50 + i * platformGap;
-    platformObject.bottom = platformBottom;
-    platform.style.bottom = platformBottom + "px";
-    platformLeft = Math.random() * 410;
-    platformObject.left = platformLeft;
-    platform.style.left = platformLeft + "px";
-    platformObject.platform = platform;
-    platforms.push(platformObject);
-
-    gameContainer.appendChild(platform);
-  }
-}
-
-function startGame(e) {
-  if (e.key === "Enter" && currentlyPlaying === false) {
-    message.innerHTML = "";
-    createPlatforms();
-    createDoodler();
-
-    currentlyPlaying = true;
-    setInterval(movePlatforms, 30);
-    setInterval(addDeletePlatform, 30);
-    setInterval(jump, 30);
-  }
-}
 // Functions}
